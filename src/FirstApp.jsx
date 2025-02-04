bf5c4aa21d18a40b22d1b2ff163b9765e4b73b4c
Juan Cremades <relicary@gmail.com>
2025-02-04 15:59:23 +0100

    Added PropsTypes to FirstApp

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,16 @@
+import PropTypes from 'prop-types';
+
 export const FirstApp = ({ title, subTitle}) => {
 
     return (
         <>
             <h1>{ title }</h1>
-            <p>{ subTitle + 1 }</p>
+            <p>{ subTitle }</p>
         </>
     );
 }
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.number.isRequired,
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
 
 import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp title="Hi, I am Relicary" subTitle={123} />
+        <FirstApp title='Hello, I am Relicary' subTitle={123} />
     </React.StrictMode>
 );
