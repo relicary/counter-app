b4991f0148e2edb8869875541e12fedd4003d523
Juan Cremades <relicary@gmail.com>
2025-02-04 16:56:54 +0100

    Added a the useState Hook working with the Click event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
 
-    const handleAdd = (event) => console.log(event);
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = (event) => {
+        setCounter( counter + 1);
+        // setCounter( (c) => c+1);
+    }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
 
             <button onClick={ handleAdd }>
                 +1
