94f4e7e4d84398a8b710ceed849bf93df6b91309
Juan Cremades <relicary@gmail.com>
2025-02-04 13:24:35 +0100

    New component using <> as Fragment

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,8 @@
-import { Fragment } from 'react'
-
 export const FirstApp = () => {
     return (
-        <Fragment>
+        <>
             <h1>Relicary</h1>
             <p>I am a subtitle</p>
-        </Fragment>
+        </>
     );
 }
