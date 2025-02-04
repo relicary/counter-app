f534b431f9c4b4dc8c408f4675711c02165a92fa
Juan Cremades <relicary@gmail.com>
2025-02-04 15:49:27 +0100

    Added Props from Main to FirstApp

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,19 +1,9 @@
-const getGreeting = (message, title) => {
-    return <h1>{message} {title}</h1>
-};
-
-const newMessage = {
-    message: 'Hello',
-    title: 'Relicary'
-};
-
-export const FirstApp = () => {
+export const FirstApp = ({ title, subTitle}) => {
 
     return (
         <>
-            { getGreeting(newMessage.message, newMessage.title) }
-            {/* <h1>{JSON.stringify(newMessage)}</h1> */}
-            <p>I am a subtitle</p>
+            <h1>{ title }</h1>
+            <p>{ subTitle + 1 }</p>
         </>
     );
 }
