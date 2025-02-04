11d2edd937310884948743e38a2706dea4718b0d
Juan Cremades <relicary@gmail.com>
2025-02-04 17:08:54 +0100

    Added habdleSubstration and handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,19 +5,21 @@ export const CounterApp = ({ value }) => {
 
     const [ counter, setCounter ] = useState( value );
 
-    const handleAdd = (event) => {
-        setCounter( counter + 1);
-        // setCounter( (c) => c+1);
-    }
+    const handleAdd = () => setCounter( counter + 1);
+    // setCounter( (c) => c+1);
+    
+    const handleSubstract = () => setCounter( counter - 1);
+
+    const handleReset = () => setCounter( value );
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
 
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleSubstract }>-1</button>
+            <button onClick={ handleReset }>Reset</button>
         </>
     );
 }
