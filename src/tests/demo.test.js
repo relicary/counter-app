9fb01cae96f2bb67b759692b81ea4fa78575b251
Juan Cremades <relicary@gmail.com>
2025-02-04 17:51:41 +0100

    Added a dump test with JEST

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,6 +1,15 @@
-test('This test shoud not fail', () => {
-    if ( 1 === 0 ) {
-        throw new Error('Zero cannot be divided between zero.');
-        
-    }
-})
+describe('Test in <DemoComponent />', () => { 
+
+    test('This test shoud not fail', () => {
+        // 1. Arrange
+        const message1 = 'Hello World';
+    
+        // 2. Act
+        const message2 = message1.trim();
+    
+        // 3. Assert
+        expect( message1 ).toBe( message2 );
+    })
+
+ })
+
