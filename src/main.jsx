65155b714232eaadf248d558da8f5964ce344dda
Juan Cremades <relicary@gmail.com>
2025-02-04 16:31:49 +0100

    Added CounterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+
+import { CounterApp } from './CounterApp';
 
 import './styles.css'
 
+
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={ '10' } />
     </React.StrictMode>
 );
