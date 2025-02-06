844f651bdfeb7fc9fac4a29ed36ffd67993e8423
Juan Cremades <relicary@gmail.com>
2025-02-06 10:52:11 +0100

    JEST: Added a test for 07-deses-arr

--- src/tests/base-pruebas/07-deses-arr.test.js
+++ src/tests/base-pruebas/07-deses-arr.test.js
@@ -6,6 +6,13 @@ describe('Test of 07-deses-arr', () => {
 
         const [ letters, numbers ] = returnArray();
 
-        expect().toBe()
+        expect( letters ).toBe('ABC');
+        expect( numbers ).toBe(123);
+
+        expect( typeof letters).toBe('string');
+        expect( typeof numbers).toBe('number');
+
+        expect( letters ).toEqual(expect.any(String));
+        expect( numbers ).toEqual(expect.any(Number));
     })
  })
