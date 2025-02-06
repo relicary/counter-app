84c07924c678d9146abe409945b4ec604467c660
Juan Cremades <relicary@gmail.com>
2025-02-06 12:58:13 +0100

    JEST: Added a test for 09-promises

---
+++ src/tests/base-pruebas/09-promises.test.js
@@ -0,0 +1,39 @@
+import { getHeroeById } from "../../base-pruebas/08-imp-exp";
+import { getHeroeByIdAsync } from "../../base-pruebas/09-promesas";
+
+describe('Test of 09-promises', () => { 
+    
+    test('getHeroeByIdAsync should return a Hero', ( done ) => {
+        
+        const id = 1;
+
+        getHeroeByIdAsync( id ).then( hero => {
+
+            expect( hero ).toEqual({
+                id: 1,
+                name: 'Batman',
+                owner: 'DC'
+            });
+
+            done();
+        });
+        
+    });
+
+    test('getHeroeByIdAsync should fail due to an Worng ID', ( done ) => {
+        
+        const id = 99;
+
+        getHeroeByIdAsync( id ).then( hero => {
+            expect( hero ).toBeFalsy();
+        })
+        .catch( error => {
+
+            expect( error ).toBe(`Hero could not be found ${ id }`);
+
+            done();
+        });
+        
+    });
+      
+ }); 
