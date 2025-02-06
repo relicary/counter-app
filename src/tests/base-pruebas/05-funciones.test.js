b0567493f60741aa0a825c69c7e42a124e0f563c
Juan Cremades <relicary@gmail.com>
2025-02-06 10:26:40 +0100

    JEST: Added a test for 05-funciones

---
+++ src/tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,31 @@
+import { getActiveUser, getUser } from "../../base-pruebas/05-funciones";
+
+describe('Test of 05-funciones', () => { 
+
+    test('getUser must return an object', () => { 
+        
+        const testUser = {
+            uid: 'ABC123',
+            username: 'relicary'
+        };
+
+        const user = getUser();
+
+        expect( testUser ).toStrictEqual( user );
+
+    });
+
+    test('getActiveUser must return an object', () => {
+        
+        const username = 'Relicary';
+        
+        const user = getActiveUser( username );
+
+        expect( user ).toStrictEqual({
+            uid: 'ABC567',
+            username: username
+        });
+
+    })
+
+ })
