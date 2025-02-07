68126b6b01a0a4ddb56c4ce14500a3cfb7a66b9d
Juan Cremades <relicary@gmail.com>
2025-02-07 12:32:29 +0100

    Enable CounterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 
 import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        {/* <CounterApp value={ 10 } />*/}
-        <FirstApp title='Hello, I am Relicary' subTitle='This is a subtitle'/>
+        <CounterApp value={ 10 } />
+        { /* <FirstApp title='Hello, I am Relicary' subTitle='This is a subtitle'/> */}
     </React.StrictMode>
 );
