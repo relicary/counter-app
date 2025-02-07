e881cc1d604cb826ba685e1d60aa65e3e0523322
Juan Cremades <relicary@gmail.com>
2025-02-07 14:03:58 +0100

    Added more DOM Test for CounterApp component

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { CounterApp } from "../src/CounterApp"
 
 describe('Test of <CounterApp />', () => { 
@@ -14,7 +14,7 @@ describe('Test of <CounterApp />', () => {
         expect( container ).toMatchSnapshot();
      });
 
-     test('Initial counter value should be 100', () => {
+    test('Initial counter value should be 100', () => {
 
         render(
             <CounterApp value={ initialValue } />
@@ -24,12 +24,71 @@ describe('Test of <CounterApp />', () => {
             screen.getByRole("heading", { level: 2 }).innerHTML
         )
         .toContain('100');
-                
+
         expect(
             screen.getByText(100)
         )
         .toBeTruthy();
      });
 
+     test('Should increment value with +1 button', () => {
+        
+        render(
+            <CounterApp value={ initialValue } />
+        );
+
+        fireEvent.click( screen.getByText('+1') );
+
+        expect( screen.getByText( initialValue + 1 ) ).toBeTruthy();
+
+     });
+
+     test('Should decrement value with -1 button', () => {
+        
+        render(
+            <CounterApp value={ initialValue } />
+        );
+
+        fireEvent.click( screen.getByText('-1') );
+
+        expect( screen.getByText( initialValue - 1 ) ).toBeTruthy();
+
+     });
+
+     test('Should reset value with Reset button', () => {
+        
+        render(
+            <CounterApp value={ initialValue } />
+        );
+
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+
+        fireEvent.click( screen.getByText('Reset') );
+
+        expect( screen.getByText( initialValue ) ).toBeTruthy();
+
+     });
+
+     test('Should reset value with Reset button', () => {
+        
+        render(
+            <CounterApp value={ initialValue } />
+        );
+
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+
+        fireEvent.click(
+            screen.getByRole('button', { name: 'btn-reset' })
+        );
+
+        expect( screen.getByText( initialValue ) ).toBeTruthy();        
+
+     });
+
  })
+ 
  
